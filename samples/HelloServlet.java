26c65a6a12af3999d4129eaa6a5bdfcbdfdc7423
Matthew Berg <mberg@synacor.com>
2017-09-28 13:58:44 -0400

    Clean up imports in samples.

--- samples/HelloServlet.java
+++ samples/HelloServlet.java
@@ -1,24 +1,9 @@
 import java.io.IOException;
 
-import java.util.EnumSet;
-
-import javax.servlet.DispatcherType;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.RequestLog;
-import org.eclipse.jetty.server.handler.HandlerCollection;
-import org.eclipse.jetty.server.handler.RequestLogHandler;
-import org.eclipse.jetty.servlet.ServletHandler;
-
-import com.synacor.jetty.log.txid.TxidFilter;
-
-import com.synacor.jetty.log.CustomRequestLog;
-import com.synacor.jetty.log.appender.Log4jAppender;
 
 public class HelloServlet extends HttpServlet
 {
--- samples/Log4j.java
+++ samples/Log4j.java
@@ -1,16 +1,14 @@
-import java.io.IOException;
-
-import org.apache.log4j.PropertyConfigurator;
-
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.RequestLog;
-import org.eclipse.jetty.server.handler.HandlerCollection;
-import org.eclipse.jetty.server.handler.RequestLogHandler;
-import org.eclipse.jetty.servlet.ServletHandler;
-
 import com.synacor.jetty.log.CustomRequestLog;
 import com.synacor.jetty.log.appender.Log4jAppender;
 import com.synacor.jetty.log.txid.TxidHandlerWrapper;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import org.eclipse.jetty.server.RequestLog;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.server.handler.RequestLogHandler;
+import org.eclipse.jetty.servlet.ServletHandler;
 
 
 public class Log4j
